b8a076e6903ceec9b64062e661adce46e3db2de0
Roudrini Mukherjee <89720737+RM-2002@users.noreply.github.com>
2024-02-06 22:34:42 +0530

    Update Singlepostname.jsx

--- client/src/components/singlepost/Singlepostname.jsx
+++ client/src/components/singlepost/Singlepostname.jsx
@@ -11,7 +11,7 @@ export default function Singlepostname() {
   const location = useLocation();
   const path = location.pathname.split("/")[2];
   const [post, setPost] = useState({});
-  const PF = "http://localhost:5000/images/";
+  const PF = "https://mern-blog-application.vercel.app/images/";
    const { user } = useContext(Context);
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
