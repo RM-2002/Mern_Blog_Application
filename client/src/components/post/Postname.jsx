b1f6c930716458462c395e6138a99dc79324f447
Roudrini Mukherjee <89720737+RM-2002@users.noreply.github.com>
2024-02-06 22:35:18 +0530

    Update Postname.jsx

--- client/src/components/post/Postname.jsx
+++ client/src/components/post/Postname.jsx
@@ -2,7 +2,7 @@ import "./post.css";
 import { Link } from "react-router-dom";
 
 export default function post({post}) {
-  const PF = "http://localhost:5000/images/";
+  const PF = "https://mern-blog-application.vercel.app/images/";
   return (
     <div className="post">
         {post.photo && <img className="postImg" src={PF+post.photo} alt="" />}
